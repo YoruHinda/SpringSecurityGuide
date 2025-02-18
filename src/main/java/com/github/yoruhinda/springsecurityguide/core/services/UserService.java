711f9e8e13700434af7e640c5563788966358c6e
Ruan Phablo <ruanphablo778@gmail.com>
2025-02-18 18:11:48 -0300

    Chore: User Service

---
+++ src/main/java/com/github/yoruhinda/springsecurityguide/core/services/UserService.java
@@ -0,0 +1,17 @@
+package com.github.yoruhinda.springsecurityguide.core.services;
+
+import com.github.yoruhinda.springsecurityguide.core.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
